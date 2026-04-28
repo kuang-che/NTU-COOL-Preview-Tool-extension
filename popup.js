8040bc5a9bc12ba3a591a5482ddb51ff4336af5c
kuang-che <147890473+kuang-che@users.noreply.github.com>
2026-04-28 17:12:46 +0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -3,10 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
         images: 'png, jpg, jpeg, gif, svg, webp',
         videos: 'mp4, webm, ogg, mov',
         audios: 'mp3, wav, ogg, m4a',
-        codes: 'py, v, sv, c, cpp, js, html, css, json, md, sh, java',
+        codes: 'py, v, sv, c, cpp, js, html, css, json, md, sh, java, m',
         plaintext: 'txt, log, csv',
-        archives: 'zip, rar, 7z, tgz, gz',
-        excels: 'xls, xlsx, csv',
+        archives: 'zip, tgz, gz',
+        excels: 'xls, xlsx',
         powerpoints: 'ppt, pptx'
     };
 
