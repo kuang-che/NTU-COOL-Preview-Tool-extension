cb153bfb69c52b96fed560dfc5111d39c699c862
66richard66 <145011112+66richard66@users.noreply.github.com>
2026-04-28 20:36:42 +0800

    Add files via upload

--- popup.js
+++ popup.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
         audios: 'mp3, wav, ogg, m4a',
         codes: 'py, v, sv, c, cpp, js, html, css, json, md, sh, java, m',
         plaintext: 'txt, log, csv',
+        archives: 'zip, tgz, gz',
         excels: 'xls, xlsx',
         powerpoints: 'ppt, pptx',
         jupyter: 'ipynb'
